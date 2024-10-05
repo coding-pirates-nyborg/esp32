a9698fd01ddbab3c823020a77c97033e4fca7bc0
fclante <fclante@gmail.com>
2024-10-05 11:43:04 +0200

    UPDATE .gitignore to omit build directory

--- cam-car/build/arduino/libraries/FastLED/platforms/avr/avr_millis_timer_source.cpp.d
+++
@@ -1,2 +0,0 @@
-/home/frey/coding-pirates/esp32/cam-car/build/arduino/libraries/FastLED/platforms/avr/avr_millis_timer_source.cpp.o: \
- /home/frey/Arduino/libraries/FastLED/src/platforms/avr/avr_millis_timer_source.cpp
--- cam-car/build/arduino/sketch/neo_pixel.cpp
+++
@@ -1,2 +0,0 @@
-#line 1 "/home/frey/coding-pirates/esp32/cam-car/src/arduino/neo_pixel.cpp"
-#include "neo_pixel.h"
